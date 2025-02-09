ea2052d6a9eafffa2f4e77d838b1e3b23c946d0e
Dmitry <mr.gn0m@yandex.ru>
2025-02-09 21:30:23 +0300

    remova wiki

--- down.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo -e "\033[32mDown app! \033[0m"
-docker compose -f app.compose.yml down
-
-echo -e "\033[32mDown efk stack! \033[0m"
-docker compose -f efk.compose.yml down 
